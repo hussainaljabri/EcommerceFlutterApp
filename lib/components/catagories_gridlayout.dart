d07f298a7bab24ec844ed5a34d72da55781f6b68
huSin <ibmhusin@gmail.com>
2019-05-10 01:47:39 -0700

    Added Catagories GridLayout.

---
+++ lib/components/catagories_gridlayout.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+//final List<String> _imageList = [
+//  'http://www.for-example.org/img/main/forexamplelogo.png?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+//  'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
+//  'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+//  'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
+//  'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
+//  'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80',
+//];
+//// Get grid tiles
+//List<Widget> _getTiles(List<File> iconList) {
+//  final List<Widget> tiles = <Widget>[];
+//  for (int i = 0; i < iconList.length; i++) {
+//    tiles.add(new GridTile(
+//        child: new InkResponse(
+//          enableFeedback: true,
+//          child: new Image.file(iconList[i], fit: BoxFit.cover,),
+//          onTap: () => _onTileClicked(i),
+//        )));
+//  }
+//  return tiles;
+//}
+//// Function to be called on click
+//void _onTileClicked(int index){
+//  debugPrint("You tapped on item $index");
+//}
+//
+//GridView.count(
+//  crossAxisCount: 4,
+//  childAspectRatio: 1.0,
+//  padding: const EdgeInsets.all(4.0),
+//  mainAxisSpacing: 4.0,
+//  crossAxisSpacing: 4.0,
+//  children: _getTiles(_imageList),
+//)
+
+
+
+class gridlayout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 270.0, // 135 height for 3 per row, 100 height for 4 per row
+      color: Colors.white30,
+      child: new GridView.count(
+          crossAxisCount: 3,
+          childAspectRatio: 1.0,
+          padding: const EdgeInsets.all(4.0),
+          mainAxisSpacing: 4.0,
+          crossAxisSpacing: 4.0,
+          children: <String>[
+            'http://www.for-example.org/img/main/forexamplelogo.png?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+            'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
+            'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+            'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
+            'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
+            'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+          ].map((String url) {
+            return new GridTile(
+                child: new Image.network(url, fit: BoxFit.cover));
+          }).toList()),
+    );
+
+  }
+}
